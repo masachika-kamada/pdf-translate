b6b0b46be8febb6e7353cc75a399c0c7398d0583
masachika <masachika.kmd@gmail.com>
2022-04-15 11:04:32 +0900

    deeplのapiを使ってテキスト翻訳できた

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import streamlit as st
+import deepl
+import json
 
 
 def main():
     st.markdown('# PDF Translate')
     file = st.file_uploader('翻訳したいpdfファイルをアップロードしてください', type=['pdf'])
+    text = "Brushless DC motors are widely used in industrial applications and computer peripheral devices, because of no brush and commutator, wide speed range, and relatively high efficiency"
     if file is not None:
         st.markdown(f'{file.name} をアップロードしました')
         with open("./pdf_files/src.pdf", "wb") as f:
             f.write(file.getvalue())
+
+        with open("./secret.json") as f:
+            secret = json.load(f)
+
+        KEY = secret["KEY"]
+        translator = deepl.Translator(KEY)
+        result = translator.translate_text(text, target_lang="JA")
+        translated_text = result.text
+        st.write(translated_text)
 
 
 if __name__ == '__main__':

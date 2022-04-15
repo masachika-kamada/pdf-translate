20da3af17672e18efbaf5e7d5ad8243fe081fccf
masachika <masachika.kmd@gmail.com>
2022-04-15 12:46:26 +0900

    DeepLクラスを作成

--- main.py
+++ main.py
@@ -3,23 +3,32 @@ import deepl
 import json
 
 
+class DeepL:
+    def __init__(self):
+        with open("./secret.json") as f:
+            secret = json.load(f)
+        KEY = secret["KEY"]
+        self.translator = deepl.Translator(KEY)
+
+    def translate(self, text):
+        result = self.translator.translate_text(
+            text, source_lang="EN", target_lang="JA")
+        return result.text
+
+
 def main():
     st.markdown('# PDF Translate')
     file = st.file_uploader('翻訳したいpdfファイルをアップロードしてください', type=['pdf'])
-    text = "Brushless DC motors are widely used in industrial applications and computer peripheral devices, because of no brush and commutator, wide speed range, and relatively high efficiency"
+    deepl = DeepL()
     if file is not None:
         st.markdown(f'{file.name} をアップロードしました')
         with open("./pdf_files/src.pdf", "wb") as f:
             f.write(file.getvalue())
 
-        with open("./secret.json") as f:
-            secret = json.load(f)
+        # read text from pdf
+        text = "I am happy."
 
-        KEY = secret["KEY"]
-        translator = deepl.Translator(KEY)
-        result = translator.translate_text(text, source_lang="EN", target_lang="JA")
-        translated_text = result.text
-        st.write(translated_text)
+        st.write(deepl.translate(text))
 
 
 if __name__ == '__main__':
